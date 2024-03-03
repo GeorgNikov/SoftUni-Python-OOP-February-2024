bfa66eec93aa649a934166a59094fa78ee54100c
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2024-03-03 21:01:51 +0200

    03.03.2024 Add Exams

---
+++ Exams/Python OOP Retake Exam - 22 Aug 2020/project/people/child.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+
+class Child:
+
+    def __init__(self, food_cost: int, *toys_cost):
+        self.cost = food_cost + sum(toys_cost)
+
+    def get_monthly_expense(self):
+        return self.cost * 30
