71a49feeffc186e589668666cc55c3cb6551f544
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2024-03-12 20:20:53 +0200

    12.03.2024 Add SOLID - update

--- SOLID/SOLID LAB/01_SRP/books.py
+++ SOLID/SOLID LAB/01_SRP/books.py
@@ -8,13 +8,13 @@ class Book:
         self.page = page
 
     def __str__(self):
-        return f"Book: {self.title} from {self.author}"
+        return f"Title {self.title} from {self.author}"
 
 
 class Library:
 
-    def __init__(self):
-        self.books = []
+    def __init__(self, books):
+        self.books = books
 
     def add_book(self, book):
         if book in self.books:
@@ -23,15 +23,20 @@ class Library:
 
     def remove_book(self, title):
         book = self.find_book(title)
-        if book not in self.books:
+
+        if not book:
             return f"{title} not in library"
-        else:
-            self.books.remove(book)
-            return f"{title} was removed from Library!"
+
+        self.books.remove(book[0])
+        return f"{title} was removed from Library!"
 
     def find_book(self, title):
-        book = [b.title for b in self.books if b.title == title][0]
-        return book
+        books = [b for b in self.books if b.title == title]
+
+        if not books:
+            raise ValueError("Title not found")
+
+        return books
 
 
 book1 = Book('Programming with Python', 'Svetlin Nakov')
@@ -39,9 +44,12 @@ book2 = Book('Programming with C#', 'Svetlin Nakov')
 book2.turn_page(350)
 print(book1)
 print(book2)
-library = Library()
-library.add_book(book1)
-library.add_book(book2)
+books = [book1, book2]
+library = Library(books)
+# library.add_book(book1)
+# library.add_book(book2)
 library.remove_book('Programming with C#')
 print(library.find_book('Programming with Python'))
 print(library.find_book('Programming with C#'))
+
+# NOT WORKING
