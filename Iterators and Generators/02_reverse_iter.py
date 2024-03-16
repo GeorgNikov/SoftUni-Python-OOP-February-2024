a9bf3d250ea2a3115249785015c63245c37f655a
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2024-03-16 19:42:33 +0200

    16.03.2024 Add Iterators and Generators

---
+++ Iterators and Generators/02_reverse_iter.py
@@ -0,0 +1,21 @@
+class reverse_iter:     # lowercase for judge
+
+    def __init__(self, iter_obj):
+        self.iter_obj = iter_obj
+        self.end_idx = len(self.iter_obj)
+        self.start_idx = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.end_idx > self.start_idx:
+            self.end_idx -= 1
+            return self.iter_obj[self.end_idx]
+        raise StopIteration
+
+
+reversed_list = reverse_iter([1, 2, 3, 4])
+
+for item in reversed_list:
+    print(item)
---
+++ Iterators and Generators/06_reverse_string.py
@@ -0,0 +1,8 @@
+def reverse_text(string):
+    current = len(string) - 1
+    while current >= 0:
+        yield string[current]
+        current -= 1
+
+for char in reverse_text("step"):
+    print(char, end='')
