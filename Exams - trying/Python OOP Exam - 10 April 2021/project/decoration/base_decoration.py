b89f3e01f743df536dab9915b3957d3e2ba43960
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2024-03-19 14:42:12 +0200

    Delete Exams - trying directory

--- Exams - trying/Python OOP Exam - 10 April 2021/project/decoration/base_decoration.py
+++
@@ -1,8 +0,0 @@
-class BaseDecoration:
-
-    def __init__(self, comfort: int, price: float):
-        self.comfort = comfort
-        self.price = price
-
-    def __repr__(self):
-        return f"{self.comfort} - {self.price}"
--- Exams - trying/Python OOP Retake Exam - 22 Aug 2020/project/appliances/laptop.py
+++
@@ -1,8 +0,0 @@
-from project.appliances.appliance import Appliance
-
-
-class Laptop(Appliance):
-
-    def __init__(self):
-        super().__init__(cost=1)
-
